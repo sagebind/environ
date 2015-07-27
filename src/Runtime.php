ee3fd9bbd7ee14c941cd6ee1d90267044fe8acbe
coderstephen <me@stephencoakley.com>
2015-07-27 15:23:44 -0500

    Add missing docblocks

--- src/Runtime.php
+++ src/Runtime.php
@@ -13,7 +13,7 @@ abstract class Runtime
      * runtime, not the system architecture. To check if the system is 64-bit,
      * {@see Platform::is64Bit()}.
      *
-     * Note that versions older than PHP7 did not support true 64-bit on
+     * Note that versions older than PHP 7 did not support true 64-bit on
      * Windows. As a result, this method will always return false on older
      * versions of PHP on Windows.
      *
@@ -24,11 +24,25 @@ abstract class Runtime
         return PHP_INT_SIZE === 8;
     }
 
+    /**
+     * Gets the path to the current interpreter executable.
+     *
+     * @return string The path to the current interpreter executable.
+     */
     public static function path()
     {
         return PHP_BINARY;
     }
 
+    /**
+     * Gets the version number of the runtime as a string.
+     *
+     * Note that the versioning scheme can vary between different runtimes. You
+     * may need to do additional processing on the returned string to use it for
+     * non-display purposes.
+     *
+     * @return string The runtime version string.
+     */
     public static function version()
     {
         if (self::isJPHP()) {
@@ -41,18 +55,34 @@ abstract class Runtime
         return PHP_VERSION;
     }
 
+    /**
+     * Checks if the runtime is the official PHP group interpreter.
+     *
+     * @return bool True if the runtime is a canonical interpreter, otherwise false.
+     */
     public static function isCanonical()
     {
         return !self::isHHVM() && !self::isJPHP();
     }
 
+    /**
+     * Checks if the runtime is HHVM.
+     *
+     * @return bool True if the runtime is HHVM, otherwise false.
+     */
     public static function isHHVM()
     {
         return defined('HHVM_VERSION');
     }
 
+    /**
+     * Checks if the runtime is JPHP.
+     *
+     * @return bool True if the runtime is JPHP, otherwise false.
+     */
     public static function isJPHP()
     {
+        // Not a perfectly accurate way of checking. This may need to be changed.
         return class_exists('php\lang\JavaClass', false);
     }
 
